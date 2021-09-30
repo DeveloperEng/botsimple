e9c699c0a8f05c5e4e75ca1981f5b891421c03bc
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-09-30 13:56:45 +0300

    Add files via upload

--- viber.py
+++ viber.py
@@ -10,76 +10,49 @@ login_itilium = os.environ['LoginItilium']
 password_itilium = os.environ['PasswordItilium']
 auth_key = os.environ['AuthKey']
 
+viber = Api(BotConfiguration(
+    name='Itilium-bot',
+    avatar='http://site.com/avatar.jpg',
+    auth_token=auth_key
+))
 
-@app.route('/getWebhookInfo', methods=['GET'])
-def getWebHookInfo():
-    # это только на время теста, надо скрывать в продакшене
-    ret = requests.post("https://api.telegram.org/bot" + auth_key + "/getWebhookInfo")
-    return ret.text
 
-
-@app.route('/setWebHook', methods=['GET'])
+@app.route('/', methods=['GET'])
 def setWebHook():
- #   dict_data = dict()
- #   address = request.url.replace("setWebHook", auth_key)
- #   print(address)
- #   dict_data.update({"url": address})
- #
- #    print(dict_data)
-#
- #   ret = requests.post("https://api.telegram.org/bot" + auth_key + "/setWebhook",
- #                       data=json.dumps(dict_data).encode('utf-8'),
- #                       headers={"Content-Type": "application/json"})
-     return "Done"
-  #  return ret.text
+            viber = Api(BotConfiguration(
+                name='Itilium-bot',
+                avatar='http://site.com/avatar.jpg',
+                auth_token=auth_key
+                ))
+            viber.unset_webhook()
+            viber.set_webhook(request.url)
+    except Exception as e:
+        return "Failed"
+    return "Success"
 
-
-@app.route('/removeWebHook', methods=['GET'])
-def removeWebHook():
-    dict_data = dict()
-    dict_data.update({"url": ""})
-
-    ret = requests.post("https://api.telegram.org/bot" + auth_key + "/setWebhook",
-                        data=json.dumps(dict_data).encode('utf-8'),
-                        headers={"Content-Type": "application/json"})
-
-    return ret.text
 
 
 @app.route('/' + auth_key, methods=['POST'])
 def IncomingConnectionPost():
     print("new message")
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-
-        update = json.loads(json_string)
-
-        print("UnJSONed: " + str(update))
-        print("update_id: " + str(update["update_id"]))
-        content = ''
-        try:
-            request_itilium = requests.post(address_api_itilium, data=json_string,
-                                            auth=(login_itilium, password_itilium))
-
-            if request_itilium.status_code == 200 and request_itilium.ok:
-                content = request_itilium.content
-
-                if content != 'Действие не найдено!':
-                    content = json.loads(content)
-        except:
-            print('Error getting data in Itilium')
-
-        # message_data = {  # формируем информацию для отправки сообщения
-        #     'chat_id': update['message']['chat']['id'],  # куда отправляем сообщение
-        #     'text': "I'm <b>bot</b>",  # само сообщение для отправки
-        #     'reply_to_message_id': update['message']['message_id'],
-        #     # если параметр указан, то бот отправит сообщение в reply
-        #     'parse_mode': 'HTML'  # про форматирование текста ниже
-        # }
-        #
-        # try:
-        #     requestTelegram = requests.post('https://api.telegram.org/bot' + auth_key + '/sendMessage', data=content)
-        # except:
-        #     print('Send message for user error')
+ #   if request.headers.get('content-type') == 'application/json':
+ #       json_string = request.get_data().decode('utf-8')
+#
+ #       update = json.loads(json_string)
+#
+ #       print("UnJSONed: " + str(update))
+  #      print("update_id: " + str(update["update_id"]))
+  #      content = ''
+  #      try:
+  #          request_itilium = requests.post(address_api_itilium, data=json_string,
+  #                                          auth=(login_itilium, password_itilium))
+#
+#            if request_itilium.status_code == 200 and request_itilium.ok:
+#                content = request_itilium.content
+#
+#                if content != 'Действие не найдено!':
+#                    content = json.loads(content)
+#        except:
+#            print('Error getting data in Itilium')
 
     return Response(status=200)
