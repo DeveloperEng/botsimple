b0f69855d10f4c37ff10859b83008a0f45491ff4
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-09-30 14:12:15 +0300

    Add files via upload

--- viber.py
+++ viber.py
@@ -1,6 +1,25 @@
 import os
+import time
+import random
+import datetime
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages import VideoMessage
+from viberbot.api.messages.picture_message import PictureMessage
+from viberbot.api.messages.text_message import TextMessage
+from viberbot.api.messages.keyboard_message import KeyboardMessage
+from viberbot.api.messages.rich_media_message import RichMediaMessage
 import requests
+import logging
 from flask import Flask, request, Response
+import psycopg2
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
+from viberbot.api.viber_requests import ViberDeliveredRequest
+
 import json
 
 app = Flask(__name__)
